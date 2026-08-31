552a6ea74e7ab0e254a711ec0b22beadd70aa63c
kadamshubham01654-cmyk <kadamshubham01654@gmail.com>
2026-08-31 10:10:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,22 @@
-const CACHE_NAME = "mh-warriors-v1";
-
-const APP_FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+const CACHE_NAME = 'mh-warriors-v1';
+const urlsToCache = [
+  './index.html',
+  './mh-logo.jpg',
+  './manifest.json'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
